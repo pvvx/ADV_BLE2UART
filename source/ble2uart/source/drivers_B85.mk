3fb4589ee70b84f8cc4fa6e1330dc686c75da834
Ircama <Ircama>
2026-05-01 19:34:15 +0200

    SDK not included in repo: use install_sdk.sh or install_sdk_v4023.sh
    - Remove SDK/ and TLSR825xScanner.bin from repository tracking
    - Add SDK/ and *.bin/*.elf to .gitignore
    - Add install_sdk_v4023.sh: installs SDK V4.0.2.3 headers/drivers from GitHub,
      automatically replaces liblt_8258.a with V4.0.2.1 (workaround for Coded PHY
      crash bug introduced in V4.0.2.2)
    - source/ble.c: Coded PHY scan window set to 500ms for maximum air time coverage
      (vs 30ms for 1M PHY, giving ~94% of radio time to Coded PHY)
    
    Tested combination: SDK V4.0.2.3 headers + liblt_8258.a V4.0.2.1

---
+++ source/ble2uart/source/drivers_B85.mk
@@ -0,0 +1,27 @@
+OUT_DIR += /drivers/B85 /drivers/B85/driver_ext
+
+OBJS += \
+$(OUT_PATH)/drivers/B85/adc.o \
+$(OUT_PATH)/drivers/B85/aes.o \
+$(OUT_PATH)/drivers/B85/analog.o \
+$(OUT_PATH)/drivers/B85/audio.o \
+$(OUT_PATH)/drivers/B85/bsp.o \
+$(OUT_PATH)/drivers/B85/clock.o \
+$(OUT_PATH)/drivers/B85/flash.o \
+$(OUT_PATH)/drivers/B85/gpio.o \
+$(OUT_PATH)/drivers/B85/i2c.o \
+$(OUT_PATH)/drivers/B85/lpc.o \
+$(OUT_PATH)/drivers/B85/qdec.o \
+$(OUT_PATH)/drivers/B85/s7816.o \
+$(OUT_PATH)/drivers/B85/spi.o \
+$(OUT_PATH)/drivers/B85/timer.o \
+$(OUT_PATH)/drivers/B85/uart.o \
+$(OUT_PATH)/drivers/B85/driver_ext/rf_pa.o \
+$(OUT_PATH)/drivers/B85/driver_ext/ext_calibration.o \
+$(OUT_PATH)/drivers/B85/watchdog.o
+
+
+# Each subdirectory must supply rules for building sources it contributes
+$(OUT_PATH)/drivers/B85/%.o: $(TEL_PATH)/drivers/B85/%.c
+	@echo 'Building driver rfile: $<'
+	@$(TC32_PATH)tc32-elf-gcc $(GCC_FLAGS) $(INCLUDE_PATHS) -c -o"$@" "$<"
